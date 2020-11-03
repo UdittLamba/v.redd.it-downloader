35e1b2056844225586fcc7e5df82de30676af15a
uditt.lamba@gmail.com <uditt.lamba@gmail.com>
2020-11-03 23:40:30 +0530

    v.redd.it implementation almost done. TODO: documentation, testing and code cleanup.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const { downloadVredditVideo } = require('./CaptureVideo')
 downloadVredditVideo(
-  'https://old.reddit.com/r/PublicFreakout/comments/jkdnfl/florida_maga_man_heckles_pete_buttigieg_who',
+  'https://old.reddit.com/r/instantkarma/comments/jn48d2/shouldnt_have_done_that/',
   './tests').catch(console.log)
